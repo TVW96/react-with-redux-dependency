9f19454fa9f917593a0e80c0b40b93e255c6fb4f
Troy Washington <troyvw96@gmail.com>
2025-02-02 20:57:15 -0800

    complete counter app features: increment, decrement, incrementByAmount, AddAsync

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount, incrementAsync, selectCount } from './counterSlice'
+import styles from './Counter.module.css'
+
+export function Counter() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+    const [incrementAmount, setIncrementAmount] = useState('2');
+
+    return (
+        <div>
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+                <input
+                    className={styles.textbox}
+                    aria-label="Set increment amount"
+                    value={incrementAmount}
+                    type="number"
+                    onChange={e =>
+                        setIncrementAmount(e.target.value)
+                    }
+                />
+                <button
+                    className={styles.button}
+                    onClick={() =>
+                        dispatch(incrementByAmount(Number(incrementAmount) || 0))
+                    }
+                >
+                    Add Amount
+                </button>
+                <button
+                    className={styles.asyncButton}
+                    onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))
+
+                    }
+                >Add Async</button>
+            </div>
+        </div>
+    )
+}
